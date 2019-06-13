c8915d9e22c7bb8ef3a4a41d68adf2e2cfa75d0c
Eric M. Johnson <eric.johnson@housecallpro.com>
2019-06-12 21:24:49 -0700

    Fix test to show with and without environment set

--- tests/terraform-0.12.x/main.tf
+++ tests/terraform-0.12.x/main.tf
@@ -13,11 +13,31 @@ resource "random_id" "name" {
   prefix      = "terraform-aws-lambda-test-0_12_X"
 }
 
-module "lambda" {
+module "lambda_with_env_vars" {
   source = "../../"
 
-  function_name = "${random_id.name.hex}"
-  description   = "Test terraform-aws-lambda with Terraform 0.12.x"
+  function_name = "${random_id.name.hex}-with-env-vars"
+  description   = "Test terraform-aws-lambda with Terraform 0.12.x with env vars"
+  handler       = "lambda.lambda_handler"
+  runtime       = "python3.6"
+  timeout       = 30
+
+  environment = {
+    variables = {
+      foo = "1"
+      bar = "zombo"
+      baz = 123
+    }
+  }
+
+  source_path = "${path.module}/lambda.py"
+}
+
+module "lambda_without_env_vars" {
+  source = "../../"
+
+  function_name = "${random_id.name.hex}-without-env-vars"
+  description   = "Test terraform-aws-lambda with Terraform 0.12.x without env vars"
   handler       = "lambda.lambda_handler"
   runtime       = "python3.6"
   timeout       = 30
