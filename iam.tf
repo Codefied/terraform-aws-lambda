272146c24363ea8b2b3371761086539fab4fe048
Eric M. Johnson <eric.johnson@housecallpro.com>
2020-03-12 14:36:11 -0700

    More quotes

--- iam.tf
+++ iam.tf
@@ -13,9 +13,9 @@ data "aws_iam_policy_document" "assume_role" {
 }
 
 resource "aws_iam_role" "lambda" {
-  name               = "${var.function_name}"
-  assume_role_policy = "${data.aws_iam_policy_document.assume_role.json}"
-  tags               = "${var.tags}"
+  name               = var.function_name
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+  tags               = var.tags
 }
 
 # Attach a policy for logs.
@@ -23,11 +23,11 @@ resource "aws_iam_role" "lambda" {
 locals {
   lambda_log_group_arn      = "arn:${data.aws_partition.current.partition}:logs:*:${data.aws_caller_identity.current.account_id}:log-group:/aws/lambda/${var.function_name}"
   lambda_edge_log_group_arn = "arn:${data.aws_partition.current.partition}:logs:*:${data.aws_caller_identity.current.account_id}:log-group:/aws/lambda/us-east-1.${var.function_name}"
-  log_group_arns            = ["${slice(list(local.lambda_log_group_arn, local.lambda_edge_log_group_arn), 0, var.lambda_at_edge ? 2 : 1)}"]
+  log_group_arns            = [slice(list(local.lambda_log_group_arn, local.lambda_edge_log_group_arn), 0, var.lambda_at_edge ? 2 : 1)]
 }
 
 data "aws_iam_policy_document" "logs" {
-  count = "${var.enable_cloudwatch_logs ? 1 : 0}"
+  count = var.enable_cloudwatch_logs ? 1 : 0
 
   statement {
     effect = "Allow"
@@ -54,24 +54,24 @@ data "aws_iam_policy_document" "logs" {
 }
 
 resource "aws_iam_policy" "logs" {
-  count = "${var.enable_cloudwatch_logs ? 1 : 0}"
+  count = var.enable_cloudwatch_logs ? 1 : 0
 
   name   = "${var.function_name}-logs"
   policy = data.aws_iam_policy_document.logs[count.index].json
 }
 
 resource "aws_iam_policy_attachment" "logs" {
-  count = "${var.enable_cloudwatch_logs ? 1 : 0}"
+  count = var.enable_cloudwatch_logs ? 1 : 0
 
   name       = "${var.function_name}-logs"
-  roles      = ["${aws_iam_role.lambda.name}"]
-  policy_arn = "${aws_iam_policy.logs[count.index].arn}"
+  roles      = [aws_iam_role.lambda.name]
+  policy_arn = aws_iam_policy.logs[count.index].arn
 }
 
 # Attach an additional policy required for the dead letter config.
 
 data "aws_iam_policy_document" "dead_letter" {
-  count = "${var.attach_dead_letter_config ? 1 : 0}"
+  count = var.attach_dead_letter_config ? 1 : 0
 
   statement {
     effect = "Allow"
@@ -82,24 +82,24 @@ data "aws_iam_policy_document" "dead_letter" {
     ]
 
     resources = [
-      "${lookup(var.dead_letter_config, "target_arn", "")}"
+      lookup(var.dead_letter_config, "target_arn", "")
     ]
   }
 }
 
 resource "aws_iam_policy" "dead_letter" {
-  count = "${var.attach_dead_letter_config ? 1 : 0}"
+  count = var.attach_dead_letter_config ? 1 : 0
 
   name   = "${var.function_name}-dl"
   policy = data.aws_iam_policy_document.dead_letter[count.index].json
 }
 
 resource "aws_iam_policy_attachment" "dead_letter" {
-  count = "${var.attach_dead_letter_config ? 1 : 0}"
+  count = var.attach_dead_letter_config ? 1 : 0
 
   name       = "${var.function_name}-dl"
-  roles      = ["${aws_iam_role.lambda.name}"]
-  policy_arn = "${aws_iam_policy.dead_letter[count.index].arn}"
+  roles      = [aws_iam_role.lambda.name]
+  policy_arn = aws_iam_policy.dead_letter[count.index].arn
 }
 
 # Attach an additional policy required for the VPC config
@@ -121,33 +121,33 @@ data "aws_iam_policy_document" "network" {
 }
 
 resource "aws_iam_policy" "network" {
-  count = "${var.attach_vpc_config ? 1 : 0}"
+  count = var.attach_vpc_config ? 1 : 0
 
   name   = "${var.function_name}-network"
-  policy = "${data.aws_iam_policy_document.network.json}"
+  policy = data.aws_iam_policy_document.network.json
 }
 
 resource "aws_iam_policy_attachment" "network" {
-  count = "${var.attach_vpc_config ? 1 : 0}"
+  count = var.attach_vpc_config ? 1 : 0
 
   name       = "${var.function_name}-network"
-  roles      = ["${aws_iam_role.lambda.name}"]
-  policy_arn = "${aws_iam_policy.network[count.index].arn}"
+  roles      = [aws_iam_role.lambda.name]
+  policy_arn = aws_iam_policy.network[count.index].arn
 }
 
 # Attach an additional policy if provided.
 
 resource "aws_iam_policy" "additional" {
-  count = "${var.attach_policy ? 1 : 0}"
+  count = var.attach_policy ? 1 : 0
 
-  name   = "${var.function_name}"
-  policy = "${var.policy}"
+  name   = var.function_name
+  policy = var.policy
 }
 
 resource "aws_iam_policy_attachment" "additional" {
-  count = "${var.attach_policy ? 1 : 0}"
+  count = var.attach_policy ? 1 : 0
 
-  name       = "${var.function_name}"
-  roles      = ["${aws_iam_role.lambda.name}"]
-  policy_arn = "${aws_iam_policy.additional[count.index].arn}"
+  name       = var.function_name
+  roles      = [aws_iam_role.lambda.name]
+  policy_arn = aws_iam_policy.additional[count.index].arn
 }
